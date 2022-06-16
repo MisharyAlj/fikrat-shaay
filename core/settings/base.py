1c828ede56e88188a9a7209d65ed8c0c01699f7c
Mishary Aljarie <mesh5874@gmail.com>
2022-06-16 23:07:29 +0300

    Change default language to arabic.

--- core/settings/base.py
+++ core/settings/base.py
@@ -120,7 +120,7 @@ SITE_ROOT = os.path.dirname(os.path.realpath(__name__))
 
 LOCALE_PATHS = (os.path.join(SITE_ROOT, 'locale'), )
 
-LANGUAGE_CODE = 'en'  # 'en-us'
+LANGUAGE_CODE = 'ar'  # 'en-us'
 
 TIME_ZONE = 'UTC'
 
